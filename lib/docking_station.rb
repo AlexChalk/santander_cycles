780c9216afdb3044f07d638ef19e0a8562eb28a9
Alex Chalk <alexchalk17@gmail.com>
2017-04-14 13:36:08 -0400

    Separates @bikes into @working_bikes and @broken_bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,21 +4,28 @@ require './lib/bike.rb'
 class DockingStation
 
   attr_reader :capacity
-  attr_accessor :bikes
+  attr_accessor :working_bikes, :broken_bikes
 
   DEFAULT_CAPACITY = 20
 
   def initialize(capacity = DEFAULT_CAPACITY)
     @capacity = capacity
-    @bikes = []
+    @working_bikes = []
+    @broken_bikes = []
   end
 
   def release_bike
-    empty? ? raise('Error: no bikes available at this docking station.') : bikes.pop
+    raise('Error: no bikes available at this docking station.') if empty?
+    working_bikes.pop
   end
 
   def dock(bike)
-    full? ? raise('Error: this docking station is occupied.') : bikes << bike
+    raise('Error: this docking station is occupied.') if full?
+    bike.broken ? broken_bikes << bike : working_bikes << bike
+  end
+
+  def bikes
+    working_bikes + broken_bikes
   end
 
   private
@@ -28,7 +35,7 @@ class DockingStation
   end
 
   def empty?
-    bikes.empty?
+    working_bikes.empty?
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -25,12 +25,13 @@ describe DockingStation do
       subject.dock(Bike.new)
       expect(subject.release_bike).to be_an_instance_of Bike
     end
-    it 'returns working bikes' do
-      subject.dock(Bike.new)
-      expect(subject.release_bike).to be_working
-    end
     it 'raises an exception when user tries to release a bike from an empty docking station' do
-      expect { subject.release_bike}.to raise_error 'Error: no bikes available at this docking station.'
+      expect{ subject.release_bike}.to raise_error 'Error: no bikes available at this docking station.'
+    end
+    it 'doesn\'t return broken bikes' do
+      bike = Bike.new; bike.report_broken
+      subject.dock(bike)
+      expect{ subject.release_bike }.to raise_error 'Error: no bikes available at this docking station.'
     end
   end
 
