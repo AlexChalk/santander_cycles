5ed4df676e886c73a113c0d52bd4a30b7e53aea8
Alex Chalk <alexchalk17@gmail.com>
2017-04-14 14:01:18 -0400

    Implements bike doubles in docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -21,30 +21,36 @@ describe DockingStation do
   end
 
   describe '#release_bike' do
+    let(:bike) { double :bike }
     it 'returns a docked bike' do
-      subject.dock(Bike.new)
-      expect(subject.release_bike).to be_an_instance_of Bike
+      allow(bike).to receive(:broken).and_return(false)
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
     end
     it 'raises an exception when user tries to release a bike from an empty docking station' do
       expect{ subject.release_bike}.to raise_error 'Error: no bikes available at this docking station.'
     end
     it 'doesn\'t return broken bikes' do
-      bike = Bike.new; bike.report_broken
+      #bike = Bike.new; 
+      allow(bike).to receive(:broken).and_return(true)
       subject.dock(bike)
       expect{ subject.release_bike }.to raise_error 'Error: no bikes available at this docking station.'
     end
   end
 
   describe '#dock' do
-    let(:bike) { Bike.new }
+    let(:bike) { double :bike }
     let(:station) { DockingStation.new(35) }
+    before(:example) do
+      allow(bike).to receive(:broken).and_return(true)
+    end
     it 'adds a bike to the docking_station\'s @bikes array' do
       subject.dock(bike)
       expect(subject.bikes).to include bike
     end
     it 'raises an exception when a user tries to dock a bike at a full docking station' do
-      DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
-      expect { subject.dock(Bike.new)}.to raise_error 'Error: this docking station is occupied.'
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock(bike) }
+      expect { subject.dock(bike)}.to raise_error 'Error: this docking station is occupied.'
     end
     it "allows a different number of bikes to be docked when a custom @capacity has been set for the DockingStation" do
       35.times { station.dock(bike) }
